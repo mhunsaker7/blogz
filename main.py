50996007f61e05974c1ef24d158b3308ba473dc0
mhunsaker7 <mhunsaker7@gmail.com>
2017-06-27 21:42:54 -0700

    up to case 1

--- main.py
+++ main.py
@@ -32,16 +32,16 @@ def index():
         title = request.form['title']
         body = request.form['body']
         if title == '':
-            flash('Please enter a title for your new blog post')
-            return redirect('/newpost')
+            flash('Please enter a title for your new blog post', 'error')
+            return render_template('newpost.html', body=body)
         if body == '':
-            flash('Please enter a body for your new blog post')
-            return redirect('/newpost')
+            flash('Please enter a body for your new blog post', 'error')
+            return render_template('newpost.html', title=title)
         blog = Blog(title=title, body=body)
         db.session.add(blog)
         db.session.commit()
         return redirect("/blog")
-
+    
     return render_template('newpost.html')
 
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RU'
